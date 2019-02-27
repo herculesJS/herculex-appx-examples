8832704465439a5dc53b869cad6d365d41c7f7ab
candy.zheng <caifeng.zxq@antfin.com>
2019-02-28 00:03:23 +0800

    update

--- quick-start/.tea/new-entryFiles-development/index$.web.js
+++
@@ -1,5 +0,0 @@
-require('@alipay/appx-compiler/lib/sjsEnvInit.js');
-require('./config$.js');
-require('../../node_modules/mini-antui/es/list/index.js');
-require('../../node_modules/mini-antui/es/list/list-item/index.js');
-require('../../pages/counter/index.js');
